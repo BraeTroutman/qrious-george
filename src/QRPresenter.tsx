094990742bdfcee41ed071477ce6a55a63b64cb0
BraeTroutman <btroutma@redhat.com>
2024-02-02 19:21:40 -0500

    encapsulate whole component in a car

--- src/QRPresenter.tsx
+++ src/QRPresenter.tsx
@@ -1,62 +1,67 @@
-import { Bullseye, Button, Flex, FlexItem, Form, FormGroup, Modal, ModalVariant, Stack, StackItem, TextInput } from "@patternfly/react-core";
+import { 
+  Bullseye, 
+  Card, CardBody, CardHeader, 
+  Divider, 
+  Flex, FlexItem, 
+  Form, FormGroup, 
+  Stack, StackItem, 
+  TextInput, 
+  Title 
+} from "@patternfly/react-core";
 import { QRCodeCanvas } from "qrcode.react";
-import { Fragment, FunctionComponent, useState } from "react";
-import { ChromePicker, ColorChangeHandler, ColorResult } from "react-color";
+import { FunctionComponent, useState } from "react";
+import { CompactPicker, ColorChangeHandler, ColorResult } from "react-color";
 
 const QRPresenter: FunctionComponent = () => {
-  const [text, setText] = useState("hey hey Dayday")
+  const [text, setText] = useState("")
   const [fg, setfg] = useState('black')
   const [bg, setbg] = useState('white')
-  const [fgPickerOpen, setFgPickerOpen] = useState(false)
-  const [bgPickerOpen, setBgPickerOpen] = useState(false)
 
   const handlePickedFgColor: ColorChangeHandler = (color: ColorResult, _) => {setfg(color.hex)}
   const handlePickedBgColor: ColorChangeHandler = (color: ColorResult, _) => {setbg(color.hex)}
-  const handleFGModalToggle = () => {setFgPickerOpen(!fgPickerOpen)}
-  const handleBGModalToggle = () => {setBgPickerOpen(!bgPickerOpen)}
 
   return (
-    <Fragment>
-    <Stack hasGutter>
-      <StackItem>
-        <Bullseye>
-          <QRCodeCanvas value={text} size={300} bgColor={bg} fgColor={fg}/>
-        </Bullseye>
-      </StackItem>
-      <StackItem>
-        <Flex>
-          <FlexItem>
-            <Button isBlock onClick={handleFGModalToggle}>Foreground Color</Button>
-          </FlexItem>
-          <FlexItem>
-            <Button isBlock onClick={handleBGModalToggle}>Background Color</Button>
-          </FlexItem>
-        </Flex>
-      </StackItem>
-      <StackItem>
-        <Form>
-          <FormGroup label="Text to encode">
-            <TextInput
-              isRequired
-              type='text'
-              value={text}
-              onChange={(_, newtext) => setText(newtext)}
-            />
-          </FormGroup>
-        </Form>
-      </StackItem>
-    </Stack>
-    <Modal isOpen={fgPickerOpen} variant={ModalVariant.small} onClose={handleFGModalToggle}>
-      <Bullseye>
-        <ChromePicker color={fg} onChangeComplete={handlePickedFgColor}/>
-      </Bullseye>
-    </Modal>
-    <Modal isOpen={bgPickerOpen} variant={ModalVariant.small} onClose={handleBGModalToggle}>
-      <Bullseye>
-        <ChromePicker color={bg} onChangeComplete={handlePickedBgColor}/>
-      </Bullseye>
-    </Modal>
-    </Fragment>
+    <Card isRounded>
+      <CardHeader><Title headingLevel="h1">Generate QR Code from Text</Title></CardHeader>
+      <Divider inset={{default: "insetMd"}}/>
+      <CardBody>
+        <Stack hasGutter>
+          <StackItem>
+            <Form>
+              <FormGroup label="Text to encode">
+                <TextInput
+                  isRequired
+                  placeholder="insert text"
+                  type='text'
+                  value={text}
+                  onChange={(_, newtext) => setText(newtext)}
+                />
+              </FormGroup>
+            </Form>
+          </StackItem>
+          <StackItem>
+              <Bullseye>
+                <Flex>
+                  <FlexItem><QRCodeCanvas value={text} size={300} bgColor={bg} fgColor={fg}/></FlexItem>
+                  <FlexItem alignSelf={{default: 'alignSelfCenter'}}>
+                    <Stack hasGutter>
+                      <Title headingLevel="h4">Foreground Color</Title>
+                      <StackItem>
+                        <CompactPicker color={fg} onChangeComplete={handlePickedFgColor}/>
+                      </StackItem>
+                      <Divider inset={{default: "insetMd"}}/>
+                      <Title headingLevel="h4">Background Color</Title>
+                      <StackItem>
+                        <CompactPicker color={bg} onChangeComplete={handlePickedBgColor}/>
+                      </StackItem>
+                    </Stack>
+                  </FlexItem>
+                </Flex>
+              </Bullseye>
+          </StackItem>
+        </Stack>
+      </CardBody>
+    </Card>
   )
 }
 
